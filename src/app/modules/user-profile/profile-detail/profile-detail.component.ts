b7b93abb6dfd69d6d2c4a971e1f59ab8833d1c7f
KaneKopetski <kane.kopetski@gmail.com>
2020-10-08 23:44:29 -0400

    Refactor profile detail to use correct url property and remove unused imports

--- src/app/modules/user-profile/profile-detail/profile-detail.component.ts
+++ src/app/modules/user-profile/profile-detail/profile-detail.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {UserProfileService} from '../user-profile.service';
-import {UserProfileResponse} from '../user-profile-response';
-import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserProfileService } from '../user-profile.service';
+import { UserProfileResponse } from '../user-profile-response';
 
 @Component({
   selector: 'app-profile-detail',
@@ -12,15 +11,12 @@ import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 export class ProfileDetailComponent implements OnInit {
 
   profile: UserProfileResponse;
-  profileAvatar: SafeResourceUrl;
-  userToken: string;
 
-  constructor(private route: ActivatedRoute, private profileService: UserProfileService, private router: Router, private domSanitizer: DomSanitizer) { }
+  constructor(private route: ActivatedRoute, private profileService: UserProfileService, private router: Router) { }
 
   ngOnInit(): void {
     const uid = this.route.snapshot.paramMap.get('uid');
     this.getProfile(uid);
-    this.userToken = sessionStorage.getItem('userToken');
   }
 
   // TODO: Look at adding an error message to 404 component to provide user more context
