2b9abfffb099b485d5069dd697d19b41989234c4
KaneKopetski <kane.kopetski@gmail.com>
2020-10-12 00:14:43 -0400

    Create character template list component and extract it from character landing

--- src/app/modules/character-builder/character-builder.module.ts
+++ src/app/modules/character-builder/character-builder.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CharactersLandingComponent } from './characters-landing/characters-landing.component';
 import { SharedModule } from '../../shared/shared.module';
+import { CharacterTemplateListComponent } from './character-template-list/character-template-list.component';
 
 
 
 @NgModule({
   declarations: [
-    CharactersLandingComponent
+    CharactersLandingComponent,
+    CharacterTemplateListComponent
   ],
   imports: [
     SharedModule
